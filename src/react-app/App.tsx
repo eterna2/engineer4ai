deca79a523a814403971ac77c8ab703e429784bb
eterna2 <eterna2@hotmail.com>
2025-06-20 01:01:57 +0800

    wip: initial chat interface

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,15 +1,59 @@
 // src/App.tsx
 
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
 import honoLogo from "./assets/hono.svg";
-import "./App.css";
 
-function App() {
+import { useWebLLM } from "./useWebLLM";
+import { ChatCompletion } from "@mlc-ai/web-llm";
+import Button from "@mui/material/Button";
+import Home from "./Home";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+
+export default function App() {
+  return (
+    <Fragment>
+      <CssBaseline enableColorScheme />
+      <Container
+        maxWidth="lg"
+        sx={{
+          py: 2,
+          height: "calc(100vh - 64px)",
+        }}
+      >
+        <Home />
+      </Container>
+    </Fragment>
+  );
+}
+
+function Home2() {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("unknown");
+  const [response, setResponse] = useState<ChatCompletion | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const { engine, progress } = useWebLLM();
+  const onClickChat = async () => {
+    if (!engine) return;
+    const messages = [
+      { role: "user", content: "How does WebLLM use workers?" },
+    ];
+    setIsLoading(true);
+    engine.chat.completions
+      .create({ messages })
+      .then(setResponse)
+      .catch(setError)
+      .finally(() => setIsLoading(false));
+  };
+
+  const chatHint = engine
+    ? "Click to run the WebLLM worker and get a response."
+    : progress?.text || "Loading WebLLM engine...";
 
   return (
     <>
@@ -58,9 +102,24 @@ function App() {
           Edit <code>worker/index.ts</code> to change the name
         </p>
       </div>
+      <div className="card">
+        <Button
+          disabled={!engine || isLoading}
+          onClick={onClickChat}
+          aria-label="get response from worker"
+        >
+          Webworker: {isLoading ? "waiting for response ..." : chatHint}
+        </Button>
+      </div>
+
+      <div style={{ maxWidth: "70vw" }}>
+        <pre style={{ whiteSpace: "pre-wrap" }}>
+          {response?.choices[0].message.content}
+        </pre>
+        <p>{error}</p>
+      </div>
+
       <p className="read-the-docs">Click on the logos to learn more</p>
     </>
   );
 }
-
-export default App;
---
+++ src/react-app/Chat.tsx
@@ -0,0 +1,85 @@
+import { ChatCompletion } from "@mlc-ai/web-llm";
+import { Fragment, useState } from "react";
+import { useWebLLM } from "./useWebLLM";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+import TextField from "@mui/material/TextField";
+import EngineeringIcon from "@mui/icons-material/Engineering";
+import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+
+export type ChatMessage =
+  | { choices: { message: { role: string; content: string } }[] }
+  | ChatCompletion;
+
+export interface ChatProps {
+  modelId?: string;
+  onResponse: (response: ChatMessage) => void;
+}
+export default function Chat({ modelId, onResponse }: ChatProps) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [content, setContent] = useState("");
+  const [error, setError] = useState(null);
+
+  const { engine, progress } = useWebLLM(modelId);
+  const onClick = async () => {
+    if (!engine) return;
+    const messages = [{ role: "user", content: content }];
+    setIsLoading(true);
+    onResponse({ choices: [{ message: { role: "user", content: content } }] });
+    setContent("");
+    engine.chat.completions
+      .create({ messages })
+      .then((reply) => {
+        onResponse(reply);
+      })
+      .catch(setError)
+      .finally(() => setIsLoading(false));
+  };
+
+  return (
+    <Fragment>
+      <Backdrop
+        sx={(theme) => ({ color: "#fff", zIndex: theme.zIndex.drawer + 1 })}
+        open={!engine}
+      >
+        <CircularProgress color="inherit" sx={{ mx: 2 }} />
+        <small>{progress ? progress.text : "Loading WebLLM engine..."}</small>
+      </Backdrop>
+
+      <TextField
+        id="chat-input"
+        label={
+          <Typography variant="h6" sx={{ mb: 2 }}>
+            <EngineeringIcon
+              fontSize="small"
+              sx={{ verticalAlign: "middle", mr: 1 }}
+            />
+            Engineer4ai
+          </Typography>
+        }
+        error={!!error}
+        helperText={error ? String(error) : ""}
+        placeholder="Type your question here..."
+        multiline
+        fullWidth
+        rows={3}
+        maxRows={10}
+        variant="standard"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <Tooltip title="Click to see loading">
+        <IconButton
+          onClick={onClick}
+          loading={isLoading}
+          disabled={!engine || isLoading || content.trim().length === 0}
+        >
+          <ArrowUpwardIcon />
+        </IconButton>
+      </Tooltip>
+    </Fragment>
+  );
+}
